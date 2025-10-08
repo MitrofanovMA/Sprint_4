c11783f8e2e65d8be58a12069de9c67e535348e7
Mikhail <Mikhail.24.Mitrofanov@yandex.ru>
2025-10-08 22:56:09 +0300

    add text parametrs for check FAQ

--- src/test/java/FaqTest.java
+++ src/test/java/FaqTest.java
@@ -10,23 +10,24 @@ import static org.junit.Assert.assertTrue;
 public class FaqTest extends BaseTest {
 
     private final int questionNumber;
+    private final String expectedAnswerPart;
 
-    public FaqTest(int questionNumber) {
+    public FaqTest(int questionNumber, String expectedAnswerPart) {
         this.questionNumber = questionNumber;
+        this.expectedAnswerPart = expectedAnswerPart;
     }
 
     @Parameterized.Parameters
     public static Collection<Object[]> data() {
-
         return Arrays.asList(new Object[][]{
-                {0},
-                {1},
-                {2},
-                {3},
-                {4},
-                {5},
-                {6},
-                {7}
+                {0, "Сутки — 400 рублей. Оплата курьеру — наличными или картой."},
+                {1, "Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим."},
+                {2, "Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30."},
+                {3, "Только начиная с завтрашнего дня. Но скоро станем расторопнее."},
+                {4, "Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010."},
+                {5, "Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. Зарядка не понадобится."},
+                {6, "Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои."},
+                {7, "Да, обязательно. Всем самокатов! И Москве, и Московской области."}
         });
     }
 
@@ -35,7 +36,6 @@ public class FaqTest extends BaseTest {
         MainPage mainPage = new MainPage(driver);
         mainPage.open();
         mainPage.acceptCookies();
-
 
         System.out.println("Тестируем вопрос №" + (questionNumber + 1));
 
@@ -52,7 +52,13 @@ public class FaqTest extends BaseTest {
         assertTrue("Текст ответа на вопрос " + (questionNumber + 1) + " слишком короткий",
                 answerText.length() > 10);
 
-        System.out.println("Вопрос " + (questionNumber + 1) + " ответ: " +
-                (answerText.length() > 50 ? answerText.substring(0, 50) + "..." : answerText));
+        assertTrue("Текст ответа на вопрос " + (questionNumber + 1) + " не соответствует ожидаемому.\n" +
+                        "Ожидалось: " + expectedAnswerPart + "\n" +
+                        "Фактически: " + answerText,
+                answerText.contains(expectedAnswerPart));
+
+        System.out.println("Вопрос " + (questionNumber + 1) + " - ответ соответствует ожидаемому");
+        System.out.println("Фактический ответ: " + answerText);
+        System.out.println("Ожидаемый ответ: " + expectedAnswerPart);
     }
 }
